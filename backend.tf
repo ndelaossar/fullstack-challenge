a722149d0cb7f0bb3202e9b5e70d3f693c19c15f
J0k3r <ndelaossar@hotmail.com>
2024-08-12 22:06:38 -0500

    Fix Node JS Pipeline (#2)
    * testing new pipeline
    
    * fix node js version
    
    * install prettier
    
    * testing prettier
    
    * add terraform code
    
    * fix format
    
    * Test deployment
    
    * test path
    
    * fir format
    
    * deploy changes
    
    ---------
    
    Co-authored-by: Nayib de la Ossa <nayib.delaossa@dualbootpartners.com>

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket               = "tf-backend-fullstack"
+    key                  = "terraform.tfstate"
+    region               = "us-east-1"
+    workspace_key_prefix = "app"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,75 @@
+resource "aws_s3_bucket" "fullstack" {
+  bucket        = "fullstack-labs-s3-${var.env_tf}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "fullstack_versioning" {
+  bucket = aws_s3_bucket.fullstack.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "fullstack_owner" {
+  bucket = aws_s3_bucket.fullstack.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "fullstack_public_access" {
+  bucket = aws_s3_bucket.fullstack.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "fullstack_acl" {
+  bucket = aws_s3_bucket.fullstack.id
+  acl    = "public-read"
+
+  depends_on = [aws_s3_bucket_ownership_controls.fullstack_owner, aws_s3_bucket_public_access_block.fullstack_public_access]
+}
+
+resource "aws_s3_bucket_website_configuration" "fullstack_website_enable" {
+  bucket = aws_s3_bucket.fullstack.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "fullstack_allow_public_access" {
+  bucket = aws_s3_bucket.fullstack.id
+  policy = data.aws_iam_policy_document.fullstack_allow_public_access_policy.json
+}
+
+data "aws_iam_policy_document" "fullstack_allow_public_access_policy" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.fullstack.arn}/*",
+    ]
+  }
+}
+
+# Sync Content
+resource "aws_s3_bucket_object" "object" {
+  for_each = fileset("build/", "**/*.*")
+  bucket   = aws_s3_bucket.fullstack.id
+  key      = each.value
+  source   = "build/${each.value}"
+}
